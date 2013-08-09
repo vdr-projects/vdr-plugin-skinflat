676860a60d13f8c6c3a23ac132ef9dd0fccd17f2
Martin Schirrmacher <vdr.skinflat@schirrmacher.eu>
2013-08-09 21:35:18 +0200

    release version 0.1.0

--- flat.h
+++ flat.h
@@ -22,13 +22,13 @@ extern cTheme Theme;
 #define CLR_MESSAGEERROR                    0xFFBB0000
 
 // TopBar
-THEME_CLR(Theme, clrTopBarBg,               0xCC101010);
+THEME_CLR(Theme, clrTopBarBg,               0xB0101010);
 THEME_CLR(Theme, clrTopBarFont,             0xFFEEEEEE);
 THEME_CLR(Theme, clrTopBarTimeFont,         0xFFEEEEEE);
 THEME_CLR(Theme, clrTopBarDateFont,         0xFFA0A0A0);
 
 // Buttons
-THEME_CLR(Theme, clrButtonBg,               0xCC101010);
+THEME_CLR(Theme, clrButtonBg,               0xB0101010);
 THEME_CLR(Theme, clrButtonFont,             0xFFEEEEEE);
 THEME_CLR(Theme, clrButtonRed,              CLR_BUTTONRED);
 THEME_CLR(Theme, clrButtonGreen,            CLR_BUTTONGREEN);
@@ -36,7 +36,7 @@ THEME_CLR(Theme, clrButtonYellow,           CLR_BUTTONYELLOW);
 THEME_CLR(Theme, clrButtonBlue,             CLR_BUTTONBLUE);
 
 // Messages
-THEME_CLR(Theme, clrMessageBg,              0xCC101010);
+THEME_CLR(Theme, clrMessageBg,              0xB0101010);
 THEME_CLR(Theme, clrMessageFont,            0xFFEEEEEE);
 
 THEME_CLR(Theme, clrMessageStatus,          CLR_MESSAGESTATUS);
@@ -45,59 +45,59 @@ THEME_CLR(Theme, clrMessageWarning,         CLR_MESSAGEWARNING);
 THEME_CLR(Theme, clrMessageError,           CLR_MESSAGEERROR);
 
 // Channel
-THEME_CLR(Theme, clrChannelBg,              0xCC101010);
+THEME_CLR(Theme, clrChannelBg,              0xB0101010);
 THEME_CLR(Theme, clrChannelFontTitle,       0xFF3090B0);
 THEME_CLR(Theme, clrChannelFontEpg,         0xFFEEEEEE);
 THEME_CLR(Theme, clrChannelFontEpgFollow,   0xFFA0A0A0);
 THEME_CLR(Theme, clrChannelProgressFg,      0xFF3090B0);
 THEME_CLR(Theme, clrChannelProgressBarFg,   0xFF3090B0);
-THEME_CLR(Theme, clrChannelProgressBg,      0xCC101010);
+THEME_CLR(Theme, clrChannelProgressBg,      0xB0101010);
 
 // Menu
-THEME_CLR(Theme, clrItemBg,                 0xCC909090);
+THEME_CLR(Theme, clrItemBg,                 0xB0909090);
 THEME_CLR(Theme, clrItemFont,               0xFFEEEEEE);
-THEME_CLR(Theme, clrItemCurrentBg,          0xCC3090B0);
+THEME_CLR(Theme, clrItemCurrentBg,          0xB03090B0);
 THEME_CLR(Theme, clrItemCurrentFont,        0xFFEEEEEE);
-THEME_CLR(Theme, clrItemSelableBg,          0xCC101010);
+THEME_CLR(Theme, clrItemSelableBg,          0xB0101010);
 THEME_CLR(Theme, clrItemSelableFont,        0xFFEEEEEE);
 THEME_CLR(Theme, clrScrollbarFg,            0xFF3090B0);
 THEME_CLR(Theme, clrScrollbarBarFg,         0xFF3090B0);
-THEME_CLR(Theme, clrScrollbarBg,            0xCC101010);
+THEME_CLR(Theme, clrScrollbarBg,            0xB0101010);
 // Menu Event
-THEME_CLR(Theme, clrMenuEventBg,            0xCC101010);
+THEME_CLR(Theme, clrMenuEventBg,            0xB0101010);
 THEME_CLR(Theme, clrMenuEventFontTitle,     0xFF3090B0);
 THEME_CLR(Theme, clrMenuEventFontInfo,      0xFFEEEEEE);
 // Menu Recording
-THEME_CLR(Theme, clrMenuRecBg,              0xCC101010);
+THEME_CLR(Theme, clrMenuRecBg,              0xB0101010);
 THEME_CLR(Theme, clrMenuRecFontTitle,       0xFF3090B0);
 THEME_CLR(Theme, clrMenuRecFontInfo,        0xFFEEEEEE);
 // Menu Text (Multiline)
-THEME_CLR(Theme, clrMenuTextBg,             0xCC101010);
+THEME_CLR(Theme, clrMenuTextBg,             0xB0101010);
 THEME_CLR(Theme, clrMenuTextFont,           0xFFEEEEEE);
 
 // Replay
-THEME_CLR(Theme, clrReplayBg,               0xCC101010);
+THEME_CLR(Theme, clrReplayBg,               0xB0101010);
 THEME_CLR(Theme, clrReplayFont,             0xFFEEEEEE);
 THEME_CLR(Theme, clrReplayFontSpeed,        0xFF3090B0);
 THEME_CLR(Theme, clrReplayProgressFg,       0xFFEEEEEE);
 THEME_CLR(Theme, clrReplayProgressBarFg,    0xFFEEEEEE);
 THEME_CLR(Theme, clrReplayProgressBarCurFg, 0xFF3090B0);
-THEME_CLR(Theme, clrReplayProgressBg,       0xCC101010);
+THEME_CLR(Theme, clrReplayProgressBg,       0xB0101010);
 THEME_CLR(Theme, clrReplayMarkFg,           0xFFEEEEEE);
 THEME_CLR(Theme, clrReplayMarkCurrentFg,    0xFF3090B0);
 
 // Tracks
-THEME_CLR(Theme, clrTrackItemBg,            0xCC101010);
+THEME_CLR(Theme, clrTrackItemBg,            0xB0101010);
 THEME_CLR(Theme, clrTrackItemFont,          0xFFEEEEEE);
-THEME_CLR(Theme, clrTrackItemCurrentBg,     0xCC3090B0);
+THEME_CLR(Theme, clrTrackItemCurrentBg,     0xB03090B0);
 THEME_CLR(Theme, clrTrackItemCurrentFont,   0xFFEEEEEE);
 
 // Volume
-THEME_CLR(Theme, clrVolumeBg,               0xCC101010);
+THEME_CLR(Theme, clrVolumeBg,               0xB0101010);
 THEME_CLR(Theme, clrVolumeFont,             0xFFEEEEEE);
 THEME_CLR(Theme, clrVolumeProgressFg,       0xFF3090B0);
 THEME_CLR(Theme, clrVolumeProgressBarFg,    0xFF3090B0);
-THEME_CLR(Theme, clrVolumeProgressBg,       0xCC101010);
+THEME_CLR(Theme, clrVolumeProgressBg,       0xB0101010);
 
 class cFlat : public cSkin {
     private:
